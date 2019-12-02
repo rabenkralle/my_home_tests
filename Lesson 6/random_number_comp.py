6402081050dae7a37ed5098b77d14aa95cc63276
Rashid Imanov <r.imanov@smun.spb.ru>
2019-12-02 11:31:45 +0300

    lesson 6 done and upgraded

--- Lesson 6/random_number_comp.py
+++ Lesson 6/random_number_comp.py
@@ -10,19 +10,38 @@ def ask_human_number(question, LOW, HIGH):
         response = int(input(question))
     return response
 
-
-
 def main():
-    tries = 10
+    tries = 0
     human_number = ask_human_number("Загадайте число", LOW, HIGH)
     cpu_number = random.randint(LOW, HIGH)
     print(cpu_number)
     guess_number = 0
-    for i in range(tries):
-        if cpu_number > human_number:
-            cpu_number = random.randint(human_number, guess_number)
-        elif cpu_number < human_number:
-            cpu_number = random.randint(guess_number, human_number)
+    guess_low = 1
+    guess_high = 100
+
+    while cpu_number != human_number:
+        human_help = input("Больше или меньше компьютерное число")
+        guess_number = cpu_number
+        tries += 1
+        if tries == 10:
+            print("Компьютер не справился")
+            break
+        if human_help == ">":
+            guess_high = cpu_number
+            cpu_number = random.randint(guess_low, guess_number)
+            print(cpu_number)
+        elif human_help == "<":
+            guess_low = cpu_number
+            cpu_number = random.randint(guess_number, guess_high)
+            print(cpu_number)
+        else:
+            print("Такого нет варианта")
+        if cpu_number == human_number:
+            print("Компьютер угадал число", human_number, "и уложился в", tries,"попыток")
+
+
+main()
+
 
 
 
