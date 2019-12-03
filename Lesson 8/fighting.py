3b2056c2cc61f846e05927cfdf796916667e903d
Rashid Imanov <r.imanov@smun.spb.ru>
2019-12-03 11:28:04 +0300

    lesson 8 upgrade

--- Lesson 8/fighting.py
+++ Lesson 8/fighting.py
@@ -9,41 +9,43 @@ damage = 50. ### Поэкспериментируйте с значениями 
 
 
 # Запрашиваем имя игрока
-
 def ask_name(question):
     name = ""
     while name == "":
         name = input(question)
     return name
 
-
+# Определеяем характеристики игрока
 def player(health, damage):
     name = ask_name("Введите имя игрока: ")
     player_stat = {"name": name, "health": health, "damage": damage}
     return player_stat
 
+# Определяем характеристики врага
 def enemy(health, damage):
     name = ask_name("Введите имя врага: ")
     enemy_stat = {"name": name, "health": health, "damage": damage}
     return enemy_stat
 
+# Описываем функцию атаки
 def attack(person1, person2):
-    health = person1["health"]
-    damage = person2["damage"]
+    health = person2["health"]
+    damage = person1["damage"]
     while health != 0:
         health -= damage
         break
     else:
         print("Герой мертв")
-    person1["health"] = health
+    person2["health"] = health
     return person1
 
+# Основная часть программы
 def main():
     human_player = player(100, 30)
     print(human_player)
     human_enemy = enemy(50, 50)
     print(human_enemy)
-    attack(human_enemy, human_player)
+    attack(human_player, human_enemy)
     print(human_player)
     print(human_enemy)
 
