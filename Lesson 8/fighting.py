306b173bd22f3e608083f4e224e619e434dbaef9
Rashid Imanov <r.imanov@smun.spb.ru>
2019-12-03 12:02:53 +0300

    lesson 8 done

--- Lesson 8/fighting.py
+++ Lesson 8/fighting.py
@@ -36,23 +36,24 @@ def enemy(health, damage, armor):
     enemy_stat = {"name": name, "health": health, "damage": damage, "armor": armor}
     return enemy_stat
 
+def get_damage(damage, armor):
+    return damage/armor
+
 # Описываем функцию атаки
 def attack(person1, person2):
-    health = person2["health"]
-    damage = person1["damage"]/person2["armor"]
-    while health != 0:
-        health -= damage
+    damage = get_damage(person1["damage"], person2["armor"])
+    while person2["health"] != 0:
+        person2["health"] -= damage
         break
     else:
         print("Герой мертв")
-    person2["health"] = health
     return person1
 
 # Основная часть программы
 def main():
-    human_player = player(100, 30)
+    human_player = player(100, 40, 1.2)
     print(human_player)
-    human_enemy = enemy(50, 50)
+    human_enemy = enemy(50, 50, 3)
     print(human_enemy)
     attack(human_player, human_enemy)
     print(human_player)
--- Lesson 8/home_work_2.py
+++ Lesson 8/home_work_2.py
@@ -3,9 +3,8 @@
 
 def max_number(number_1, number_2, number_3):
     # Ищем максимальное число из трех
-    numbers = [number_1, number_2, number_3]
-    big = max(numbers)
-    return big
+    result = max([number_1, number_2, number_3])
+    return result
 
 def ask_number():
     number = int(input("Введите число: "))
@@ -16,6 +15,6 @@ num_1 = ask_number()
 num_2 = ask_number()
 num_3 = ask_number()
 
-big = max_number(num_1, num_2, num_3)
-print(big)
+result = max_number(num_1, num_2, num_3)
+print(result)
 
