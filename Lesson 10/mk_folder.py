76f7a6d6e71b1375109f747ff96ba77f36a9bf36
Rashid Imanov <r.imanov@smun.spb.ru>
2019-12-04 09:42:53 +0300

    Lesson 10 upgraded

--- Lesson 10/mk_folder.py
+++ Lesson 10/mk_folder.py
@@ -6,3 +6,7 @@ def make_folder():
         new_path = os.path.join(os.getcwd(),"{}_{}".format("dir", i))
         os.mkdir(new_path)
 
+def del_folder():
+    for i in range(1, 10):
+        new_path = os.path.join(os.getcwd(),"{}_{}".format("dir", i))
+        os.removedirs(new_path)
