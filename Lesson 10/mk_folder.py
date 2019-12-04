8bb15c5d94e0170ed5a871ea186c5691496bee7e
Rashid Imanov <r.imanov@smun.spb.ru>
2019-12-04 11:38:04 +0300

    Lesson 10 second upgrade

--- Lesson 10/mk_folder.py
+++ Lesson 10/mk_folder.py
@@ -3,10 +3,10 @@ import os
 
 def make_folder():
     for i in range(1, 10):
-        new_path = os.path.join(os.getcwd(),"{}_{}".format("dir", i))
-        os.mkdir(new_path)
+        folder_name = f'dir_{i}'
+        os.mkdir(folder_name)
 
 def del_folder():
     for i in range(1, 10):
-        new_path = os.path.join(os.getcwd(),"{}_{}".format("dir", i))
-        os.removedirs(new_path)
+        folder_name = f'dir_{i}'
+        os.removedirs(folder_name)
